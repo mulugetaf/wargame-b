6c5cd384fffd0a3ab711e724e47b1d07f65cbffb
Mulugeta <muluks@gmail.com>
2020-06-04 02:42:12 +0300

    remove demo

--- Test_amichai.cpp
+++ Test_amichai.cpp
@@ -4,19 +4,19 @@
 #include "doctest.h"
 #include <iostream>
 #include <stdexcept>
+#include "DemoGame.hpp"
 #include "FootSoldier.hpp"
 #include "FootCommander.hpp"
 #include "Sniper.hpp"
 #include "SniperCommander.hpp"
 #include "Paramedic.hpp"
 #include "ParamedicCommander.hpp"
-#include "Board.hpp"
 #include <cassert>
 
 //check exceptions in diff cases
 using namespace WarGame;
 TEST_CASE("Sniper VS Sniper")
-{ 
+{ //good
     WarGame::Board board(8, 8);
     CHECK(!board.has_soldiers(1));
     board[{0, 1}] = new Sniper(1);
@@ -34,6 +34,7 @@ TEST_CASE("Sniper VS Sniper")
     CHECK(!board.has_soldiers(2));
     CHECK(board.has_soldiers(1));
 }
+
 TEST_CASE("Foot vs Foot")
 {
     WarGame::Board board(8, 8);
